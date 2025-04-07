69912b9ca1a0910aaec7e1d910c023e20106c3fd
luca <lucamarper@gmail.com>
2025-04-07 20:39:11 -0300

    sistema ventas

---
+++ SistemaVentas.js
@@ -0,0 +1,49 @@
+// Clase padre
+class DispositivoEntrada {
+    constructor(tipoEntrada, marca) {
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+
+    get tipoEntrada() {
+        return this._tipoEntrada;
+    }
+
+    set tipoEntrada(tipoEntrada) {
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca() {
+        return this._marca;
+    }
+
+    set marca(marca) {
+        this._marca = marca;
+    }
+
+    toString() {
+        return `Tipo de entrada: ${this._tipoEntrada}, Marca: ${this._marca}`;
+    }
+}
+
+
+
+
+// Clase Monitor
+class Monitor {
+    static contadorMonitores = 0;
+
+    constructor(marca, tamaño) {
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamaño = tamaño;
+    }
+
+    get idMonitor() {
+        return this._idMonitor;
+    }
+
+    toString() {
+        return `Monitor [ID: ${this._idMonitor}], Marca: ${this._marca}, Tamaño: ${this._tamaño}`;
+    }
+}
