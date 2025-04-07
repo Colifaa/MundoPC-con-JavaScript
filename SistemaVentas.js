5f5d3802c51bb34f4cd1ee4ea563a6c98e8f446f
Cami <camialvarez45@gmail.com>
2025-04-07 20:56:38 -0300

    clase raton y teclado

--- SistemaVentas.js
+++ SistemaVentas.js
@@ -26,6 +26,32 @@ class DispositivoEntrada {
     }
 }
 
+class Raton extends DispositivoEntrada {
+    static contadorRatones = 0;
+  
+    constructor(tipoEntrada, marca) {
+      super(tipoEntrada, marca);
+      this._idRaton = ++Raton.contadorRatones;
+    }
+  
+    toString() {
+      return `Raton [ID: ${this._idRaton}, Tipo Entrada: ${this.tipoEntrada}, Marca: ${this.marca}]`;
+    }
+  }
+  
+
+  class Teclado extends DispositivoEntrada {
+    static contadorTeclado = 0;
+  
+    constructor(tipoEntrada, marca) {
+      super(tipoEntrada, marca);
+      this._idTeclado = ++Teclado.contadorTeclado;
+    }
+  
+    toString() {
+      return `Teclado [ID: ${this._idTeclado}, Tipo Entrada: ${this.tipoEntrada}, Marca: ${this.marca}]`;
+    }
+  }
 
 
 
